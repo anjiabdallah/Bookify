87c338e1cd0692d5250f25939a05ee8397f04e60
anjiabdallah <anjiabdallah@gmail.com>
2026-05-22 02:10:52 +0300

    fix eslint issues in migration 1

--- server/src/migrations/001_create_users.ts
+++ server/src/migrations/001_create_users.ts
@@ -1,5 +1,6 @@
 import { type Kysely } from 'kysely';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable('users')
@@ -11,6 +12,7 @@ export async function up(db: Kysely<any>): Promise<void> {
     .execute();
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function down(db: Kysely<any>): Promise<void> {
   await db.schema.dropTable('users').execute();
 }
